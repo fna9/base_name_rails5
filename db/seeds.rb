3472ca54a088466ba51116f1659c83ee4de4fbc5
fna9 <bolonka94@mail.ru>
2016-06-10 08:21:27 +0300

    09/06/16

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+<<<<<<< HEAD
+users = [['Гаврелюк', 'Артемий', 'Андреевич', 'gaa120@mail.msiu.ru'], 
+['Фомин', 'Валерий', 'Алексеевич', 	'fva12@mail.msiu.ru'],
+['Фомина', 'Наталья', 'Александровна', 	'fna9@mail.msiu.ru'],
+['Хараборчев', 'Илья', 'Сергеевич', 	'xis4@mail.msiu.ru'],
+['Салкин', 'Никита', 'Юрьевич', 	'snq13@mail.msiu.ru'],
+['Опекунов', 'Сергей', 'Дмитриевич', 	'osd2@mail.msiu.ru'],
+['Стрельская', 'Дарья', 'Александровна', 	'sda102@mail.msiu.ru'],
+['Умурзакова', 'Алина', 'Станиславовна', 	'uak2@mail.msiu.ru'],
+['Мишин', 'Андрей', 'Дмитриевич', 	'mad19@mail.msiu.ru'],
+['Сикачев', 'Евгений', 'Александрович', 	'sea83@mail.msiu.ru'],
+['Седых', 'Дмитрий', 'Владимирович', 	'sdv39@mail.msiu.ru'],
+['Никандрова', 'Ксения', 'Сергеевна', 	'nks8@mail.msiu.ru'], 
+['Татаринов', 'Сергей', 'Александрович', 	'tsa27@mail.msiu.ru'], 
+['Хачковский', 'Алексей', 'Дмитриевич', 	'xad2@mail.msiu.ru']] 
+
+users_obj = []
+users.each do |u|
+  u1 = User.find_by_email(u[3])
+  if u1.blank?
+    u1 = User.create(email: u[3], password: "123", password_confirmation: "123")
+    u1.activation_state = 'active'
+    u1.save
+  end
+  users_obj << u1
+end
+
+=======
+>>>>>>> ccc135a1ce16c208a84b581a04c6f1afd0263dbf
 u1 = User.find_by_email("bolonka94@gmail.com")
 if u1.blank?
   u1 = User.create(email: "bolonka94@gmail.com", password: "123", password_confirmation: "123")
@@ -13,9 +42,15 @@ if u1.blank?
   u1.save
 end
 
+<<<<<<< HEAD
+u2 = User.find_by_email("radigin@yandex.ru")
+if u2.blank?
+  u2 = User.create(email: "radigin@yandex.ru", password: "321", password_confirmation: "321")
+=======
 u2 = User.find_by_email("bolonka94@mail.ru")
 if u2.blank?
   u2 = User.create(email: "bolonka94@mail.ru", password: "321", password_confirmation: "321")
+>>>>>>> ccc135a1ce16c208a84b581a04c6f1afd0263dbf
   u2.activation_state = 'active'
   u2.save
 end
@@ -30,19 +65,35 @@ RoleUser.create(role: r1, user: u1)
 RoleUser.create(role: r2, user: u1)
 RoleUser.create(role: r3, user: u1)
 
+<<<<<<< HEAD
+RoleUser.create(role: r1, user: u2)
+=======
 RoleUser.create(role: r3, user: u2)
+>>>>>>> ccc135a1ce16c208a84b581a04c6f1afd0263dbf
 
 uni1 = University.create(name: 'МАМИ', full_name: "Московский государственный машиностроительный университет")
 uni2 = University.create(name: 'МГИУ', full_name: "Московский государственный индустриальный университет")
 
 t1 = Teacher.create(last_name: 'Белова', first_name: 'Ирина', second_name: 'Михайловна', b_date: Date.new(1953, 03, 16), degree: 'к.т.н.', rank: 'Доцент', position: 'Заведующая кафедрой')
 t2 = Teacher.create(last_name: 'Иванова', first_name: 'Елена', second_name: 'Михайловна', b_date: Date.new(1989, 07, 23), degree: '-', rank: '-', position: 'Старший преподаватель')
-t3 = Teacher.create(last_name: 'Мурзаев', first_name: 'Григорий', second_name: 'Дмитриевич', b_date: Date.new(1980, 05, 03), degree: 'к.т.н.', rank: 'Доцент', position: 'Старший преподаватель')
+t3 = Teacher.create(last_name: 'Радыгин', first_name: 'Виктор', second_name: 'Юрьевич', b_date: Date.new(1980, 05, 03), degree: 'к.т.н.', rank: 'Доцент', position: 'Доцент', user: u2)
 t4 = Teacher.create(last_name: 'Петров', first_name: 'Денис', second_name: 'Сергеевич', b_date: Date.new(1972, 07, 29), degree: '-', rank: '-', position: 'Преподаватель')
 t5 = Teacher.create(last_name: 'Александрова', first_name: 'Нина', second_name: 'Григорьевна', b_date: Date.new(1989, 10, 9), degree: '-', rank: '-', position: 'Ассистент')
+<<<<<<< HEAD
+t6 = Teacher.create(last_name: 'Фомина', first_name: 'Наталья', second_name: 'Александровна', b_date: Date.new(1994, 10, 9), degree: '-', rank: '-', position: 'Ассистент', user: u2)
+
+#puts t1.errors.inspect
+studs_obj = []
+users.each_with_index do |u, i|
+  s1 = Student.create(last_name: u[0], first_name: u[1], second_name: u[2], b_date: Date.new(1994, 10, 9), passport_id: (i.to_s*4)[0..3], passport_series: 97687, user: u1)
+  studs_obj << s1
+end
+
+=======
 t6 = Teacher.create(last_name: 'Фомина', first_name: 'Наталья', second_name: 'Александровна', b_date: Date.new(1994, 10, 9), degree: '-', rank: '-', position: 'Ассистент', user: u1)
 
 #puts t1.errors.inspect
+>>>>>>> ccc135a1ce16c208a84b581a04c6f1afd0263dbf
 s1 = Student.create(last_name: 'Фомина', first_name: 'Наталья', second_name: 'Александровна', b_date: Date.new(1994, 10, 9), passport_id: '0987', passport_series: 97687, user: u1)
 s2 = Student.create(last_name: 'Стрельская', first_name: 'Дарья', second_name: 'Александровна', b_date: Date.new(1995, 03, 14), passport_id: '0985', passport_series: 37687)
 s3 = Student.create(last_name: 'Седых', first_name: 'Дмитрий', second_name: 'Владимирович', b_date: Date.new(1994, 03, 26), passport_id: '5487', passport_series: 97187)
@@ -63,8 +114,15 @@ sb3 = Subject.create(title: 'Компьютерная графика', s_type: '
 sb4 = Subject.create(title: 'Веб-технологии', s_type: 'Семинар', type_of_reporting: 0)
 sb5 = Subject.create(title: 'Программирование', s_type: 'Лекции', type_of_reporting: 3)
 
+<<<<<<< HEAD
+c1 = Community.create(name: 'Базы Данных')
+c2 = Community.create(name: 'Основы Ruby on Rails')
+
+user
+=======
 c1 = Community.create(name: 'Название1')
 c2 = Community.create(name: 'Название2')
+>>>>>>> ccc135a1ce16c208a84b581a04c6f1afd0263dbf
 
 p1 = Page.create(visibility: true, header: 'Header1', contents: 'Contents1', alias: 'Alias1')
 p2 = Page.create(visibility: true, header: 'Header2', contents: 'Contents2', alias: 'Alias2')
